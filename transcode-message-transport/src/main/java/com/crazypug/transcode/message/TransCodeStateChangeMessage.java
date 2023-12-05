c249d2c50580723bb51cf6503266a5baad870dc6
cc <cc@cc.com>
2023-12-05 18:05:07 +0800

    转码协议

---
+++ transcode-message-transport/src/main/java/com/crazypug/transcode/message/TransCodeStateChangeMessage.java
@@ -0,0 +1,32 @@
+package com.crazypug.transcode.message;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransCodeStateChangeMessage {
+
+
+    private List<File> files;
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class File {
+        private String fileId;
+        private String fileName;
+        private String parentName;
+
+    }
+
+
+}
